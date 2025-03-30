ad8edf7a7de394516d6fbdc8a0ce6a6820e32362
gusrnjohn217 <gusrnjohn217@gmail.com>
2025-03-30 18:11:10 +0900

    URL change

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://api.sol-wms.store";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://api.solcloud.store";
 //const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 
 
